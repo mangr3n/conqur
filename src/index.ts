3c0780fcb1ddb98e179e5183b1612462cc5458de
David Boyer <mangr3n@gmail.com>
2020-08-27 19:11:43 -0400

    Working extracting code from Project

---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import { setAsap } from "./setasap";
+
+const conqurSystem = (self as any)._conqurInners = (self as any)._conqurInners || {
+  processes: {},
+  messageQueue: [],
+  messageQueueIndex: 0,
+  errors: {
+    byMessageId: {}, byProcessId: {}
+  },
+  pidCounter: 0,
+  midCounter: 0
+};
+
+const isNil = (v) => {
+  if (v === undefined) return true;
+  if (v === null) return true;
+  return false;
+}
+
+export interface Process {
+  name?: string;
+  self: () => number;
+  handleCall?: (any) => Promise<any>;
+  handleCast: (any) => void;
+}
+
+const registerProcess = (pid: number, process:Process) => {
+  conqurSystem.processes[pid] = process;
+}
+
+const isProcess = (pid: number) => {
+  return !isNil(conqurSystem.processes[pid]);
+}
+
+const queueMessage = (pid, mid, message) => {
+  conqurSystem.messageQueue.push({pid,message,mid});
+};
+
+const getNextMessage = () => {
+  const message = conqurSystem.messageQueue[conqurSystem.messageQueueIndex];
+  conqurSystem.messageQueueIndex++;
+  if (conqurSystem.messageQueueIndex == conqurSystem.messageQueue.length) {
+    conqurSystem.messageQueue = [];
+    conqurSystem.messageQueueIdx = 0;
+  } else {
+    setAsap(processEventQueue)
+  }
+  return message;
+}
+
+const registerError = (errorPacket) => {
+  const {mid, pid} = errorPacket;
+  conqurSystem.errors.byMessageId[mid] = errorPacket;
+  if ( conqurSystem.errors.byProcessId[pid] == null) {
+    conqurSystem.errors.byProcessId[pid] = [];
+  }
+  conqurSystem.errors.byProcessId[pid].push(errorPacket);
+}
+
+const processEventQueue = () => {
+  const {pid,message, mid} = getNextMessage();
+
+  if (!isProcess(pid)) {
+    throw new Error(`Cannot handle a queued message for a non-existent process: ${pid}`);
+  }
+  const process = getProcess(pid);
+  try {
+    process.handleCast(message);
+  } catch (error) {
+    registerError({message,mid,pid,error});
+  }
+};
+
+const getProcess = (pid) => {
+  if (!isProcess(pid)) {
+    throw new Error(`Cannot get a process that does not exist: ${pid}`);
+  }
+  return conqurSystem.processes[pid];
+}
+
+
+/**
+ * 1. Creates a process from the process definition, 
+ * 2. Registers the process with the conqer System
+ * @returns the process id
+ */
+export const create: (x: Process) => number = (process: Process) => {
+  const result = conqurSystem.pidCounter++;
+  process.self = () => result;
+  registerProcess(result,process);
+  return result;
+};
+
+export const destroy = (pid:number) => {
+  if (isProcess(pid)) {
+    delete conqurSystem.processes[pid];
+  } else {
+    throw new Error(`Cannot destroy a non-existent process: ${pid}`);
+  }
+};
+
+export const cast = (pid, message: any) => {
+  if (!isProcess(pid)) {
+    throw new Error(`Cannot cast a message to a non-existent process: ${pid}`);
+  }
+  const mid = conqurSystem.midCounter++;
+  queueMessage(pid,mid,message);
+  setAsap(processEventQueue);
+  return mid;
+}
+
+export async function call(pid, message) {
+  if (!isProcess(pid)) throw new Error(`Cannot call a non-existent process: ${pid}`);
+  const process = getProcess(pid);
+  return await process.call(message);
+};
---
+++ src/setasap.ts
@@ -0,0 +1,46 @@
+declare var self: any;
+// Schedules work off the stack
+// It will push to the microtask queue (processed first after stack)
+// Or if a certain amount of time has passed, it will push to the event loop
+// This allows eventhandlers and renderering to fire.
+require('setimmediate');
+
+const queueMicrotask = (() => {
+  if (self.queueMicrotask !== undefined) {
+    return self.queueMicrotask;
+  } else {
+    const unusedNodes = [];
+    return (callback) => {
+      let hiddenDiv = null;
+      if (unusedNodes.length !== 0) {
+        hiddenDiv = unusedNodes.pop();
+      } else {
+        hiddenDiv = document.createElement('div');
+      }
+      const obs = new MutationObserver(function () {
+        callback();
+        obs.disconnect();
+        unusedNodes.push(hiddenDiv);
+        hiddenDiv = null;
+      });
+      obs.observe(hiddenDiv, { attributes: true });
+      hiddenDiv.setAttribute('i', '1');
+    };
+  }
+})();
+
+const setImmediate = self.setImmediate;
+
+let lastTimeToEventLoop = null;
+// let countToTaskQueue = 0;
+export const setAsap = (callback) => {
+  if (lastTimeToEventLoop == null) {
+    lastTimeToEventLoop = Date.now();
+  }
+  if ((Date.now() - lastTimeToEventLoop) > 20) {
+    setImmediate(callback);
+    lastTimeToEventLoop = null;
+    return;
+  }
+  queueMicrotask(callback);
+};
