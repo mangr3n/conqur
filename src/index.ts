fcc54f9c848a676295fa7633f831de0f402a7233
David Boyer <mangr3n@gmail.com>
2022-03-03 11:36:44 -0500

    Added exports for all and default

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import { create, call, cast, destroy } from './core';
+import { 
+  create as _create, 
+  call as _call, 
+  cast as _cast, 
+  destroy as _destroy 
+} from './core';
 export type { Process, ProcessID, ProcessAccessor } from './types';
-import { GenServer } from './GenServer';
-import { Registry } from './Registry';
-import { State } from './State';
-import { Bus } from './Bus';
+import { GenServer as _GenServer } from './GenServer';
+import { Registry as _Registry } from './Registry';
+import { State as _State} from './State';
+import { Bus as _Bus } from './Bus';
 import _Utils from './util/index';
 
 export const Utils = _Utils;
+export const create = _create;
+export const call = _call;
+export const cast = _cast;
+export const destroy = _destroy;
+export const GenServer = _GenServer;
+export const Registry = _Registry;
+export const State = _State;
+export const Bus = _Bus;
+
 
 declare let mount: () => void;
 // Mount provides runtime access to the APIs
